caa0c881ad128b86b50d3c95568eb0e21ab52e8c
chenroy1688 <chenroy1688@gmail.com>
2021-03-29 17:11:27 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,12 @@ import Router from 'vue-router'
 import Pos from '@/components/page/Pos'
 
 //全局統一路徑
-import { publicPath } from '../../vue.config'
+// import { publicPath } from '../../vue.config'
 
 Vue.use(Router)
 
 export default new Router({
-  base:publicPath,
+  // base:publicPath,
   routes: [
     {
       path: '/',
