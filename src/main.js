54256baf0192344e2fa397fa578a7f7ab4c5113d
chenroy1688 <chenroy1688@gmail.com>
2021-03-29 16:07:02 +0800

    修改mainjs

--- src/main.js
+++ src/main.js
@@ -4,13 +4,21 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
+// import Routes from './router/index'
 import 'element-ui/lib/theme-default/index.css'
 
 //全局統一路徑
 // import { publicPath } from '../vue.config'
+// import { VueRouter } from 'vue-router/types/router'
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
+// Vue.use(VueRouter)
+
+// const router = new VueRouter({
+//   base:publicPath,
+//   routes: Routes,
+// })
 
 /* eslint-disable no-new */
 new Vue({
